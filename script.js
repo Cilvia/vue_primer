fb7bce63b6934efa2500c006433437e397782688
cilvia <tech0970@gmail.com>
2018-05-06 14:39:45 +0900

    イベント設定

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ new Vue({
         text: ''
     }
 });
+
+new Vue({
+    el: '.js-component03',
+    data: {
+        input: '',
+        text: ''
+    },
+    methods: {
+        output: function(){
+            this.text = this.input;
+        }
+    }
+});
